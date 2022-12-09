ddf1fdd38be0c7caa9e7cdf3a44c95a2119871a7
Mahesh-HC <hcm119620@gmail.com>
2022-12-09 18:26:28 +0530

    changes in mongoDB url

--- Models/bookingDetailModel.js
+++ Models/bookingDetailModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-//mongoose.connect('mongodb://localhost:27017').then((res) => console.log('connected to Ticketdb')).catch((err) => console.log('err in connection'))
-mongoose.connect('mongodb+srv://mahesh:parvathamma@cluster0.tibizpb.mongodb.net/?retryWrites=true&w=majority').then((res) => console.log('connected to Ticketdb')).catch((err) => console.log('err in connection'))
+mongoose.connect('mongodb://localhost:27017').then((res) => console.log('connected to Ticketdb')).catch((err) => console.log('err in connection'))
 const bd = mongoose.Schema
 const bdSchema = new bd({
     username: String,
--- Models/flightDetailModel.js
+++ Models/flightDetailModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
-//mongoose.connect('mongodb://localhost:27017').then((res) => console.log('connected to Flightdb')).catch((err) => console.log('err in connection'))
-mongoose.connect('mongodb+srv://mahesh:parvathamma@cluster0.tibizpb.mongodb.net/?retryWrites=true&w=majority').then((res) => console.log('connected to Flightdb')).catch((err) => console.log('err in connection'))
+mongoose.connect('mongodb://localhost:27017').then((res) => console.log('connected to Flightdb')).catch((err) => console.log('err in connection'))
 const fd = mongoose.Schema
 const fdSchema = new fd({
     flightNo: Number,
